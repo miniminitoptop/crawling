cddec59a8870769e09b8abc702d4ce39429456fd
miniminitoptop <miniminitoptop@gmail.com>
2023-08-22 19:25:12 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,6 @@
+mainkeyword = ["NottinghamForestFC","NewcastleUnitedFC","LiverpoolFC","ManchesterCityFC","ManchesterUnitedFC","BurnleyFC","AFCBournemouth","Brighton&HoveAlbionFC","BrentfordFC","SheffieldUnitedFC","ArsenalFC","AstonVillaFC","EvertonFC","WolverhamptonWanderersFC","WestHamUnitedFC","ChelseaFC","TottenhamHotspurFC","CrystalPalaceFC","FulhamFC"]
+subkeyword = "2022-2023Homekit"
+
+for i in mainkeyword:
+    keyword = i + subkeyword
+    print(keyword)
