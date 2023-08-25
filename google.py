04a5233f798755b39709b9da5e34e0112841879f
miniminitoptop <miniminitoptop@gmail.com>
2023-08-25 11:01:24 +0900

    학습 시작

--- google.py
+++ google.py
@@ -21,7 +21,7 @@ driver = webdriver.Chrome(service=Service, options=chrome_options)
 
 
 mainkeyword = ["NottinghamForestFC","NewcastleUnitedFC","LiverpoolFC","ManchesterCityFC","ManchesterUnitedFC","BurnleyFC","AFCBournemouth","Brighton&HoveAlbionFC","BrentfordFC","SheffieldUnitedFC","ArsenalFC","AstonVillaFC","EvertonFC","WolverhamptonWanderersFC","WestHamUnitedFC","ChelseaFC","TottenhamHotspurFC","CrystalPalaceFC","FulhamFC"]
-subkeyword = "2022-2023Homekit"
+subkeyword = "2022-2023"
 
 for i in mainkeyword:
 
